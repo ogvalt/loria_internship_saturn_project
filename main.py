a336eea6243c530d0cd8f71ef45f77a23889d3f6
ogvalt <ogvalt@gmail.com>
2017-03-29 13:34:54 +0200

    Evaluate amount of edges and amount of movement (pixel count)

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-
-# TODO: Preprocessing layer
+# TODO: SOM
+# TODO: Spiking SOM
 
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
+from sompy.sompy import SOMFactory
 
-cam = cv2.VideoCapture("SaturnV.mp4")
+cam = cv2.VideoCapture("lol.mp4")
 
 previous_frame_dog = None
+data_edges = np.array([])
+data_movement = np.array([])
 
 while True:
     (grabbed, frame) = cam.read()
@@ -16,10 +20,15 @@ while True:
 
     frame = cv2.resize(frame, dsize=(0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # frame_gray = cv2.GaussianBlur(frame_gray, (5, 5), 0)
+    # frame_gray = cv2.dilate(frame_gray, None, iterations=2)
 
     g1 = cv2.GaussianBlur(frame_gray, (1, 1), 0)
     g2 = cv2.GaussianBlur(frame_gray, (3, 3), 0)
     frame_dog = g1 - g2
+
+    amount_of_edges = cv2.countNonZero(frame_dog)
+    data_edges = np.append(data_edges, amount_of_edges)
 
     frame_dog = cv2.threshold(frame_dog, 100, 255, cv2.THRESH_BINARY)[1]
 
@@ -28,14 +37,24 @@ while True:
 
     frame_temporal = cv2.subtract(frame_dog, previous_frame_dog)
 
+    amount_of_movement = cv2.countNonZero(frame_temporal)
+    data_movement = np.append(data_movement, amount_of_movement)
+
     cv2.imshow("SaturnV", frame_gray)
     cv2.imshow("DoG", frame_dog)
     cv2.imshow("Temporal", frame_temporal)
 
     previous_frame_dog = frame_dog.copy()
 
-    if cv2.waitKey(40) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cam.release()
 cv2.destroyAllWindows()
+
+a = plt.plot(data_edges, data_movement, 'ob')
+
+print(data_movement, data_movement.min(), data_movement.max())
+print(data_edges, data_edges.min(), data_edges.max())
+
+plt.show(a)
